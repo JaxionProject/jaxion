09d56e88d7497c643c4a772409a41abc860c3efa
pmocz <philip.mocz@gmail.com>
2025-09-18 19:57:01 -0400

    little cleanup

--- jaxion/simulation.py
+++ jaxion/simulation.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
 from functools import partial
+import importlib.resources
 import json
 
 from .constants import constants
@@ -20,7 +21,7 @@ class Simulation:
 
     def __init__(self, params):
         # start from default simulation parameters
-        with open("params_default.json", "r") as f:
+        with importlib.resources.open_text("jaxion", "params_default.json") as f:
             self._params = json.load(f)
         # update with user params
         for key in params:
@@ -33,7 +34,7 @@ class Simulation:
         self.state = {}
         self.state["t"] = 0.0
         if self.params["physics"]["quantum"]:
-            self.state["psi"] = jnp.zeros((self.resolution, self.resolution)) * 1j
+            self.state["psi"] = jnp.zeros((self.resolution, self.resolution, self.resolution)) * 1j
 
         # XXX TODO: finish
 
@@ -41,10 +42,6 @@ class Simulation:
         if self.resolution % 2 != 0:
             raise ValueError("Resolution must be divisible by 2.")
 
-        # internal simulation state -- should not be touched by user!
-        self._internal = {}
-        if self.params["physics"]["gravity"]:
-            self._internal["V"] = jnp.zeros((self._nx, self._ny))
 
     @property
     def resolution(self):
@@ -119,42 +116,36 @@ class Simulation:
         """
 
         # Simulation parameters
-        dt = self._dt
-        nt = self._nt
-        dx = self._dx
-        vol = self._vol
+        dt = 0.1 # XXX
+        nt = 100
 
         # Fourier space variables
         if self.params["physics"]["gravity"] or self.params["physics"]["quantum"]:
             kx, ky, kz = self.fourier
             k_sq = kx**2 + ky**2 + kz**2
 
-        # initialize potential
-        if self.params["physics"]["gravity"]:
-            self._internal["V"] = self._calc_grav_potential(state, k_sq)
-
         def update(i, state):
             # Update the simulation state by one timestep
             # according to a 2nd-order `kick-drift-kick` scheme
 
             # Kick (half-step)
+            if self.params["physics"]["gravity"]:
+                V = self._calc_grav_potential(state, k_sq)
             if self.params["physics"]["quantum"] and self.params["physics"]["gravity"]:
-                state["psi"] = quantum_kick(state["psi"], self._internal["V"], dt / 2.0)
+                state["psi"] = quantum_kick(state["psi"], V, 0.5 * dt)
 
             # Drift (full-step)
             if self.params["physics"]["quantum"]:
                 state["psi"] = quantum_drift(state["psi"], k_sq, dt)
 
-            # update potential
-            if self.params["physics"]["gravity"]:
-                self._internal["V"] = self._calc_grav_potential(state, k_sq)
-
             # Kick (half-step)
+            if self.params["physics"]["gravity"]:
+                V = self._calc_grav_potential(state, k_sq)
             if self.params["physics"]["quantum"] and self.params["physics"]["gravity"]:
-                state["psi"] = quantum_kick(state["psi"], self._internal["V"], dt / 2.0)
+                state["psi"] = quantum_kick(state["psi"], V, 0.5 * dt)
 
             # update time
-            state["t"] += nt * dt
+            state["t"] += dt
 
             return state
 
