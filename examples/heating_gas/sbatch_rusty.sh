05fd1c7466b46a41b1422f1ef3c5413f301425b3
pmocz <philip.mocz@gmail.com>
2025-10-09 10:47:55 -0400

    add sbatch file

---
+++ examples/heating_gas/sbatch_rusty.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+#SBATCH --job-name=heating_gas
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH --partition gpu
+#SBATCH --constraint=h100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=80G
+#SBATCH --time=00-00:10
+
+module purge
+module load python/3.11
+
+export PYTHONUNBUFFERED=TRUE
+
+source $VENVDIR/jaxion-venv/bin/activate
+
+srun python heating_gas.py --res=4
