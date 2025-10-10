1e8aeb02e14c6ac2cd32ea2070f9a0c067ad19d4
pmocz <philip.mocz@gmail.com>
2025-10-09 20:38:58 -0400

    print number of timesteps

--- jaxion/simulation.py
+++ jaxion/simulation.py
@@ -247,7 +247,7 @@ class Simulation:
             return state
 
         # save initial state
-        print("Starting simulation ...")
+        print(f"Starting simulation (nt={nt}) ...")
         if self.params["output"]["save"]:
             with open(os.path.join(checkpoint_dir, "params.json"), "w") as f:
                 json.dump(self.params, f, indent=2)
