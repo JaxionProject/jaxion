37868b6126b5bc8271e1a721f56d4b5cf5514bb0
pmocz <philip.mocz@gmail.com>
2025-10-25 18:56:49 -0400

    soliton_binary_merger.py add distributed mode

--- examples/soliton_binary_merger/README.md
+++ examples/soliton_binary_merger/README.md
@@ -10,7 +10,7 @@ Usage:
 python soliton_binary_merger.py --res <resolution_multiplier>
 ```
 
-Takes around 6 (res=1) seconds to run on my macbook (cpu).
+Takes around 4 (res=1) seconds to run on my macbook (cpu).
 
 
 ## Simulation snapshots
--- examples/soliton_binary_merger/soliton_binary_merger.py
+++ examples/soliton_binary_merger/soliton_binary_merger.py
@@ -1,7 +1,13 @@
+import jax
 import jax.numpy as jnp
-import numpy as np
-import jaxion
+from jax.experimental import mesh_utils
+from jax.sharding import Mesh, PartitionSpec, NamedSharding
 import argparse
+import os
+import sys
+
+sys.path.append("../../")  # XXX
+import jaxion
 
 # switch on for double precision
 # jax.config.update("jax_enable_x64", True)
@@ -13,10 +19,12 @@ Philip Mocz (2025)
 
 Usage:
   python soliton_binary_merger.py --res <resolution_multiplier>
+
+  python soliton_binary_merger.py --distributed --emulate
 """
 
 
-def set_up_simulation(resolution_multiplier):
+def set_up_simulation(resolution_multiplier, sharding):
     # Parameters added/changed from default values
     params = {
         "domain": {
@@ -28,11 +36,12 @@ def set_up_simulation(resolution_multiplier):
     }
 
     # Initialize the simulation
-    sim = jaxion.Simulation(params)
+    sim = jaxion.Simulation(params, sharding=sharding)
 
     # Set initial conditions (randomly places solitons)
     m_22 = sim.params["quantum"]["m_22"]
     box_size = sim.params["domain"]["box_size"]
+    r_soliton = 0.02 * box_size
     nx = sim.resolution
     xx, yy, zz = sim.grid
 
@@ -41,7 +50,7 @@ def set_up_simulation(resolution_multiplier):
             1.9e7 * m_22**-2 * r_soliton**-4 / (1.0 + 0.091 * (r / r_soliton) ** 2) ** 8
         )
 
-    rho = np.zeros((nx, nx, nx), dtype=complex)
+    rho = 0.0
     r_soliton = 0.02 * box_size
     for i in range(2):
         x_soliton = (0.5 + 0.3 * (i - 0.5)) * box_size
@@ -54,7 +63,9 @@ def set_up_simulation(resolution_multiplier):
         rho += rho_soliton(r, r_soliton, m_22)
 
     half = (xx - 0.5 * box_size) > 0
-    sim.state["psi"] = jnp.array(jnp.sqrt(rho)) * (1.0 * half + -1.0 * (1.0 - half))
+    sim.state["psi"] = jnp.array(jnp.sqrt(rho) + 0j) * (
+        1.0 * half + -1.0 * (1.0 - half)
+    )
 
     return sim
 
@@ -62,9 +73,35 @@ def set_up_simulation(resolution_multiplier):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--res", type=int, default=1, help="Resolution multiplier")
+    parser.add_argument(
+        "--distributed", action="store_true", help="run in distributed mode"
+    )
+    parser.add_argument(
+        "--emulate", action="store_true", help="emulate distributed mode on CPU"
+    )
     args = parser.parse_args()
 
-    sim = set_up_simulation(args.res)
+    if args.distributed:
+        if args.emulate:
+            flags = os.environ.get("XLA_FLAGS", "")
+            flags += " --xla_force_host_platform_device_count=8"  # change to, e.g., 8 for testing sharding virtually
+            os.environ["CUDA_VISIBLE_DEVICES"] = ""
+            os.environ["XLA_FLAGS"] = flags
+            if jax.process_index() == 0:
+                print("Using emulated distributed CPU mode")
+        else:
+            jax.distributed.initialize()
+            if jax.process_index() == 0:
+                print("Using distributed GPU mode")
+        # Create mesh and sharding for distributed computation
+        n_devices = jax.device_count()
+        devices = mesh_utils.create_device_mesh((1, n_devices))
+        mesh = Mesh(devices, axis_names=("x", "y"))
+        sharding = NamedSharding(mesh, PartitionSpec("x", "y"))
+    else:
+        sharding = None
+
+    sim = set_up_simulation(args.res, sharding)
     sim.run()
 
     return sim
