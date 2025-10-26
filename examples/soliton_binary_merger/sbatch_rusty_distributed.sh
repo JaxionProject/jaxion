ee40ecd4ca19984efc61c2a60ba72837a784d44b
pmocz <philip.mocz@gmail.com>
2025-10-25 20:29:01 -0400

    add sbatch distributed

---
+++ examples/soliton_binary_merger/sbatch_rusty_distributed.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+#SBATCH --job-name=soliton_binary_merger
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH --partition gpu
+#SBATCH --constraint=h100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=80G
+#SBATCH --time=00-01:00
+
+module purge
+module load python/3.11
+
+export PYTHONUNBUFFERED=TRUE
+
+source $VENVDIR/jaxion-venv/bin/activate
+
+srun --cpu-bind=cores python soliton_binary_merger.py --res=32 --distributed
